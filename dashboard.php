5f13b5447f285d85e392e710c49ea8d7aecfa1a2
eezstreet <eezstreet@live.com>
2013-11-14 13:41:32 -0500

    Pointed all searches to /search.php (and made that file, too)

--- dashboard.php
+++ dashboard.php
@@ -16,7 +16,7 @@ template_start("Dashboard - JKHub API");
     <div class="container" style="width:60%;">
         <br><br> <!-- goddamn bootstrap navbars/Google Chrome -->
         <h1>Dashboard</h1>
-        <form class="form-search" action="dashsearch.php" method="post">
+        <form class="form-search" action="search.php?t=u" method="post">
             <div class="input-group">
                 <input type="text" placeholder="Find Friends" class="form-control">
                 <span class="input-group-btn">
